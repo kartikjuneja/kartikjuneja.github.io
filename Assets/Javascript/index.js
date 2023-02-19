cf8896dbe36f22da38d25c4e7cc8d30c304b5a57
kartikjuneja <kjuneja7004@gmail.com>
2023-02-20 01:54:01 +0530

    Changed almost alot

--- Assets/Javascript/index.js
+++ Assets/Javascript/index.js
@@ -5,11 +5,55 @@
     usernames.forEach(span => {
         span.textContent = username.textContent;
     });
-    const message = document.querySelector("#message");
-    const buttonRow1 = document.querySelector("#buttonRow1");
-    const button1 = document.querySelector("#button1");
+    const message = document.querySelector("#message");;
     const button3 = document.querySelector("#button3");
     const buttonRow2 = document.querySelector("#buttonRow2");
+
+    const Screens = {
+        Home: $("#HomeScreen"),
+        About: $("#AboutScreen"),
+        Education: $("#EducationScreen"),
+        Social: $("#SocialScreen")
+    };
+
+    const Buttons = {
+        ToAbout: $("#buttonAbout"),
+        ToHome_About: $("#buttonHome_About"),
+        ToEducation: $("#buttonEducation"),
+        ToSocial: $("#buttonSocial"),
+        ToAbout_Education: $("#buttonAbout_Education"),
+        ToAbout_Social: $("#buttonAbout_Social")
+    };
+
+    Buttons.ToHome_About.click(function () {
+        ToHome();
+    });
+
+    Buttons.ToAbout.click(function () {
+        ToAbout();
+    });
+
+    Buttons.ToEducation.click(function () {
+        ToEducation();
+    });
+
+    Buttons.ToAbout_Education.click(function () {
+        ToAbout();
+    });
+
+    Buttons.ToSocial.click(function () {
+        ToSocial();
+    });
+
+    Buttons.ToAbout_Social.click(function () {
+        ToAbout();
+    });
+
+    //buttonAbout.addEventListener("click", function () {
+    //    Screens.Home.hide();
+    //    Screens.About.show();
+    //    message.textContent = username.textContent + ", wanna connect with me?";
+    //});
 
     button3.addEventListener("click", function () {
         GetUserNameScreen();
@@ -63,25 +107,24 @@
 
     });
 
-    button1.addEventListener("click", function () {
-        header.style.display = "none";
-        buttonRow1.style.display = "none";
-        buttonRow2.style.display = "block";
-
-        message.textContent = username.textContent + ", wanna connect with me?";
-    });
+    //button2.addEventListener("click", function () {
+    //    header.style.display = "none";
+    //    HomeButtons.style.display = "none";
+    //    const messageText = "Thankyou for your kind visit.";
+    //    ShowJobProfiles(messageText);
+    //});
 
     button4.addEventListener("click", function () {
         const messageText = "You're really nice, " + username.textContent + ".";
         ShowSocialProfiles(messageText);
     });
 
-    buttonForYou.addEventListener("click", function () {
-        header.style.display = "none";
-        buttonRow1.style.display = "none";
-        const messageText = "Thankyou for your kind visit.";
-        ShowSocialProfiles(messageText);
-    });
+    //buttonForYou.addEventListener("click", function () {
+    //    header.style.display = "none";
+    //    HomeButtons.style.display = "none";
+    //    const messageText = "Thankyou for your kind visit.";
+    //    ShowSocialProfiles(messageText);
+    //});
 
     button5.addEventListener("click", function () {
         const messageText = "Good Choice, " + username.textContent + ".";
@@ -108,6 +151,31 @@
         }
     });
 
+    function ToHome() {
+        Screens.Home.show();
+        Screens.About.hide();
+    }
+
+    function ToAbout() {
+        Screens.Home.hide();
+        Screens.About.show();
+        Screens.Education.hide();
+        Screens.Social.hide();
+    }
+
+    function ToEducation() {
+        Screens.Home.hide();
+        Screens.About.hide();
+        Screens.Education.show();
+    }
+
+    function ToSocial() {
+        Screens.Home.hide();
+        Screens.About.hide();
+        Screens.Education.hide();
+        Screens.Social.show();
+    }
+
     function ShowSocialProfiles(messageText) {
 
         buttonRow2.style.display = "none";
@@ -119,22 +187,49 @@
         socialmedia.style.display = "block";
     }
 
+    function loadPage(href) {
+        //var iframe = document.createElement("iframe");
+        //var url = "path/to/your/html/JobProfiles.html";
+        //iframe.src = url;
+        //iframe.width = "100%";
+        //iframe.height = "500px";
+        //var container = document.getElementById("frameCon");
+        //container.appendChild(iframe);
+
+        //var xmlhttp = new XMLHttpRequest();
+        //xmlhttp.open("GET", href, false);
+        //xmlhttp.send();
+        //return xmlhttp.responseText;
+    }
+
+    function ShowJobProfiles(messageText) {
+        buttonRow2.style.display = "none";
+        buttonRow3.style.display = "block";
+        button7.style.display = "inline-block";
+
+        message.textContent = messageText;
+        socialinvite.style.display = "block";
+        jobprofiles.style.display = "block";
+        jobprofiles.innerHTML = loadPage('Pages/JobProfiles.html');
+    }
+
     function ResetAll() {
         $('#header').show();
-        $('#buttonRow1').show();
+        $('#HomeButtons').show();
         $('#buttonRow2').hide();
         $('#buttonRow3').hide();
 
         message.textContent = "What's up?";
         socialinvite.style.display = "none";
         socialmedia.style.display = "none";
+        jobprofiles.style.display = "none";
         socialmediaAnonymous.style.display = "none";
         $('#userNameDiv').hide();
     }
 
     function GetUserNameScreen() {
         $('#header').hide();
-        $('#buttonRow1').hide();
+        $('#HomeButtons').hide();
         $('#buttonRow2').hide();
         $('#buttonRow3').hide();
 
